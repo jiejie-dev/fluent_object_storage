4884f276bc4f790504ddd70d36fd7ccf9ad46dcf
jiejie-dev <jeremaihloo1024@gmail.com>
2022-07-23 03:16:07 +0800

    Update put object

--- lib/fluent_object_storage.dart
+++ lib/fluent_object_storage.dart
@@ -1,1 +1,7 @@
 library fluent_object_storage;
+
+export 'package:fluent_object_storage/put_object_event_handler.dart';
+export 'package:fluent_object_storage/put_object_progress.dart';
+export 'package:fluent_object_storage/put_object_request.dart';
+export 'package:fluent_object_storage/put_object_response.dart';
+export 'package:fluent_object_storage/put_object_result.dart';
--- lib/put_object_request.dart
+++ lib/put_object_request.dart
@@ -1,18 +1,35 @@
 /// 上传请求
 class ObjectStoragePutObjectRequest {
   /// 支持路径
-  final String file;
+  final String filePath;
   final String bucketName;
   final String objectName;
+  final String accessKeyId;
+  final String accessKeySecret;
+  final String securityToken;
+  final int expiredTime;
+  final String region;
 
   ObjectStoragePutObjectRequest(
-      {required this.file, required this.bucketName, required this.objectName});
+      {required this.filePath,
+      required this.bucketName,
+      required this.objectName,
+      required this.accessKeyId,
+      required this.accessKeySecret,
+      required this.securityToken,
+      required this.expiredTime,
+      required this.region});
 
   Map<String, dynamic> toMap() {
     return {
-      "file": file,
+      "filePath": filePath,
       "bucketName": bucketName,
       "objectName": objectName,
+      "accessKeyId": accessKeyId,
+      "accessKeySecret": accessKeySecret,
+      "securityToken": securityToken,
+      "expiredTime": expiredTime,
+      "region": region
     };
   }
 }
